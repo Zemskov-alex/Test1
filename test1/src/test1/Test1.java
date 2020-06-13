c484ea18011ae831207db9bd32369a377378fda8
Alexey <zemskovale@mail.ru>
2020-06-13 18:37:24 +0300

    Commit Test1

---
+++ test1/src/test1/Test1.java
@@ -0,0 +1,24 @@
+package test1;
+
+public class Test1 {
+	 public static void main(String[] args) {
+	        int i = 3;
+	        switch(i) {
+	            default:
+	                System.out.println("default");
+	//break;
+	            case 1:
+	                System.out.println("1");
+	//break;
+
+	            case 2:
+	                System.out.println("2");
+	                
+	            case 3:
+	            	System.out.println("JOPA");
+	//break;
+
+	        }
+	    }
+	}
+
